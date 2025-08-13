e8e4f1b44c4bb3d056d1ae50439caf5599a0ebd0
AndreaSuryatanaya <andreasemut@gmail.com>
2025-08-13 13:14:45 +0700

    setup:error/erro_mapping.go

---
+++ constants/error/user.go
@@ -0,0 +1,17 @@
+package error
+
+import "errors"
+
+var (
+	ErrUserNotFound         = errors.New("password not found")
+	ErrPasswordIncorrect    = errors.New("password incorrect")
+	ErrUsernameExist        = errors.New("username already exist")
+	ErrPasswordDoesNotMatch = errors.New("password does not match")
+)
+
+var UserErrors = []error{
+	ErrUserNotFound,
+	ErrPasswordIncorrect,
+	ErrUsernameExist,
+	ErrPasswordDoesNotMatch,
+}
