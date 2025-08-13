6951ac665b2bed7a9911e84ee5091e235a331df4
AndreaSuryatanaya <andreasemut@gmail.com>
2025-08-14 02:53:15 +0700

    middleware: create func extBarier, Unauthorized, valApiKey

---
+++ middlewares/middleware.go
@@ -0,0 +1,82 @@
+package middlewares
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"strings"
+	"user-service/common/response"
+	"user-service/config"
+	"user-service/constants"
+	errConstant "user-service/constants/error"
+
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func HandlePanic() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if r := recover(); r != nil {
+				logrus.Errorf("Recovered from panic: %v", r)
+				c.JSON(http.StatusInternalServerError, response.Response{
+					Status:  constants.Error,
+					Message: errConstant.ErrInternalServerError.Error(),
+				})
+				c.Abort()
+			}
+		}()
+		c.Next()
+	}
+}
+
+func RateLimiter(lmt *limiter.Limiter) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := tollbooth.LimitByRequest(lmt, c.Writer, c.Request)
+		if err != nil {
+			c.JSON(http.StatusTooManyRequests, response.Response{
+				Status:  constants.Error,
+				Message: errConstant.ErrTooManyRequests.Error(),
+			})
+			c.Abort()
+		}
+		c.Next()
+	}
+}
+
+func extractBearerToken(token string) string {
+	arrayToken := strings.Split(token, " ")
+	if len(arrayToken) == 2 {
+		return arrayToken[1]
+	}
+	return ""
+}
+
+func responseUnauthorized(c *gin.Context, message string) {
+	c.JSON(http.StatusUnauthorized, response.Response{
+		Status:  constants.Error,
+		Message: message,
+	})
+	c.Abort()
+}
+
+func validateAPIKey(c *gin.Context) error {
+	apiKey := c.GetHeader(constants.XApiKey)
+	requestAt := c.GetHeader(constants.XRequestAt)
+	serviceName := c.GetHeader(constants.XServiceName)
+	signatureKey := config.Config.SignatureKey
+
+	validateKey := fmt.Sprintf("%s:%s:%s", serviceName, signatureKey, requestAt)
+	hash := sha256.New()
+	hash.Write([]byte(validateKey))
+	resultHash := hex.EncodeToString(hash.Sum(nil))
+
+	if apiKey != resultHash {
+		return errConstant.ErrUnauthorized
+	}
+
+	return nil
+}
